79a4c917013e74e60d05b8229bdf487c9d80d9d4
hacker65536 <s.hacker65536@gmail.com>
2019-09-07 13:37:40 +0000

    update

---
+++ contests/abc138/abc138_b/golang/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	var a []int
+
+	fmt.Scanf("%d", &n)
+	fmt.Scanf("%#v", &a)
+	fmt.Println(n)
+	fmt.Println(a)
+
+}
---
+++ contests/practice/tasks/practice_1/README.md
@@ -0,0 +1,24 @@
+
+int a,b,c 
+string s
+
+```
+a
+b c
+s
+```
+
+input
+--
+```
+1
+2 3
+test
+```
+
+output
+--
+```
+6 test
+```
+
